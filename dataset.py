e41db62a29b0c1c69fb4cbc3ab56a26adf69d1b1
Anthony Swierkosz <anthony@swierkosz.dev>
2022-11-29 21:50:00 -0500

    Update Dataset to include image pairs of reference and subject images

--- dataset.py
+++ dataset.py
@@ -1,14 +1,38 @@
+"""
+Author: Anthony Swierkosz
+
+Used for loading the dataset and creating the training and testing sets.
+Contains Dataset class along with helper classes and functions.
+"""
+
 import os
+from enum import Enum
 
 TXT_EXT = '.txt'
+PNG_EXT = '.png'
 
 
-class Figure:
+class ImageType(Enum):
     """
-    A figure object for holding a single figure's data.
+    Enum for image types, reference image and subject image.
+    """
+    REFERENCE = 0
+    SUBJECT = 1
+
+
+class Image:
+    """
+    A single image, either a reference image or a subject image.
     """
 
-    def __init__(self, path):
+    def __init__(self, image_type, path):
+        """
+        Initialize the image.
+
+        :param image_type: Reference or subject image.
+        :param path: Path to the image excluding the extension.
+        """
+        self.type = image_type
         self.path = path
         self.gender = None
         self.clss = None
@@ -18,7 +42,7 @@ class Figure:
 
     def __load(self):
         """
-        Load the figure's text data from the path.
+        Load the image's text data based on the image's path.
 
         :return: None
         """
@@ -38,16 +62,57 @@ class Figure:
             self.history = history[history.find(':') + 1:]
 
 
+class ImagePair:
+    """
+    An Image Pair is a pair of images, a reference image and a subject image.
+    """
+
+    def __init__(self, root, figure_name):
+        """
+        Initialize the image pair.
+
+        :param root: Root directory of the image pair.
+        :param figure_name: Name the images share, generally the number.
+        """
+        self.figure_name = figure_name
+        self.reference = None
+        self.subject = None
+
+        self.__generate_pair(root)
+
+    def __generate_pair(self, root):
+        """
+        Generate a pair of images from the root and figure name.
+
+        :return: None
+        """
+        # Generate paths
+        reference_path = os.path.join(root, 'f' + self.figure_name)
+        subject_path = os.path.join(root, 's' + self.figure_name)
+
+        # Create image objects
+        self.reference = Image(ImageType.REFERENCE, reference_path)
+        self.subject = Image(ImageType.SUBJECT, subject_path)
+
+
 class Dataset:
     """
     Create a dataset object for holding all the data.
     """
 
     def __init__(self, root):
+        """
+        Initialize the dataset.
+
+        :param root: Root directory of the dataset.
+        """
         self.root = root
         self.data = []
+        self.training = []
+        self.testing = []
 
         self.__load_data()
+        self.__split_data()
         print("Done")
 
     def __load_data(self):
@@ -59,7 +124,17 @@ class Dataset:
         print("Loading data...")
         for rt, dirs, files in os.walk(self.root):
             for f_name in files:
-                if f_name.endswith(".png"):
-                    self.data.append(Figure(os.path.join(rt, f_name[:-4])))
+                if f_name.startswith('f') and f_name.endswith(PNG_EXT):
+                    # Clean File Name
+                    f_name = f_name[1:-4]
 
-        print("Done")
+                    self.data.append(ImagePair(rt, f_name))
+
+    def __split_data(self):
+        """
+        Split the data into training and testing data.
+
+        :return: None
+        """
+        self.training = self.data[:1500]
+        self.testing = self.data[1500:]
