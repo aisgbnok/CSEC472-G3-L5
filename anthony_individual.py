afcdd1e7ffc894ae63de8f1750e909bf8b3baeff
Anthony Swierkosz <anthony@swierkosz.dev>
2022-11-30 21:42:16 -0500

    Add technique 1

--- anthony_individual.py
+++ anthony_individual.py
@@ -28,7 +28,7 @@ def morphological_transformation(image):
     """
     Performs a morphological transformation on the greyscale image.
 
-    :return: The transformed image.
+    :return: The lowest and highest greyscale values from the morphological transformation.
     """
     # Create a structuring element
     eclipse = cv.getStructuringElement(cv.MORPH_ELLIPSE, (10, 10))
@@ -37,8 +37,86 @@ def morphological_transformation(image):
     lowest_gray = cv.morphologyEx(image, cv.MORPH_OPEN, eclipse)
     highest_gray = cv.morphologyEx(image, cv.MORPH_CLOSE, eclipse)
 
-    # Return the intersection of the two images
-    return lowest_gray + highest_gray
+    # Return the lowest and highest gray values
+    return lowest_gray, highest_gray
+
+
+def technique_1(dataset):
+    """
+    Anthony's Technique 1.
+    Performs a morphological transformation on the greyscale image.
+    Takes the intersection of the lowest and highest greyscale values.
+    Converts the greyscale image to a binary image.
+    Compares the binary images using the Hamming distance.
+
+    :param dataset: Dataset to use.
+    :return: None
+    """
+    # Load the first image
+    reference = load_image(dataset.training[10].reference.get_image_path())
+    subject = load_image(dataset.training[10].subject.get_image_path())
+
+    # Perform the morphological transformation
+    reference_lowest_gray, reference_highest_gray = morphological_transformation(reference)
+    subject_lowest_gray, subject_highest_gray = morphological_transformation(subject)
+
+    # Take the intersection of the lowest and highest gray values
+    # reference = cv.bitwise_xor(reference_lowest_gray, reference_highest_gray)
+    # subject = cv.bitwise_xor(subject_lowest_gray, subject_highest_gray)
+
+    # reference = reference_lowest_gray - reference_highest_gray
+    # subject = subject_lowest_gray - subject_highest_gray
+
+    intersection_reference = reference_lowest_gray + reference_highest_gray
+    intersection_subject = subject_lowest_gray + subject_highest_gray
+
+    # Binarize the images
+    binary_reference = binarize_image(intersection_reference)
+    binary_subject = binarize_image(intersection_subject)
+
+    # Show images
+    figure = plt.figure(constrained_layout=True)
+    figure.set_size_inches(10, 16)
+    figure.suptitle("Technique 1")
+    (original, morph, intersection, binary) = figure.subfigures(4, 1)
+
+    # Original
+    original.suptitle("Original Images")
+    (original_ref, original_sub) = original.subplots(1, 2)
+    original_ref.imshow(reference, cmap='gray')
+    original_ref.set_title("Reference Image")
+    original_sub.imshow(subject, cmap='gray')
+    original_sub.set_title("Subject Image")
+
+    # Morphological Transformation
+    morph.suptitle("Morphological Transformation")
+    ((morph_ref_low, morph_sub_low), (morph_ref_high, morph_sub_high)) = morph.subplots(2, 2)
+    morph_ref_low.imshow(reference_lowest_gray, cmap='gray')
+    morph_ref_low.set_title("Reference Lowest Gray")
+    morph_ref_high.imshow(reference_highest_gray, cmap='gray')
+    morph_ref_high.set_title("Reference Highest Gray")
+    morph_sub_low.imshow(subject_lowest_gray, cmap='gray')
+    morph_sub_low.set_title("Subject Lowest Gray")
+    morph_sub_high.imshow(subject_highest_gray, cmap='gray')
+    morph_sub_high.set_title("Subject Highest Gray")
+
+    # Intersection
+    intersection.suptitle("Intersection")
+    (intersection_ref, intersection_sub) = intersection.subplots(1, 2)
+    intersection_ref.imshow(intersection_reference, cmap='gray')
+    intersection_ref.set_title("Reference Image")
+    intersection_sub.imshow(intersection_subject, cmap='gray')
+    intersection_sub.set_title("Subject Image")
+
+    # Binary
+    binary.suptitle("Binary")
+    (binary_ref, binary_sub) = binary.subplots(1, 2)
+    binary_ref.imshow(binary_reference, cmap='gray')
+    binary_ref.set_title("Reference Image")
+    binary_sub.imshow(binary_subject, cmap='gray')
+    binary_sub.set_title("Subject Image")
+
+    plt.show()
 
 
 def main(dataset):
@@ -46,26 +124,10 @@ def main(dataset):
     Main method for Anthony's individual components.
 
     :param dataset: Dataset to use.
-    :return:
+    :return: None
     """
-    # Load images
-    reference_image = load_image(dataset.training[0].reference.get_image_path())
-    subject_image = load_image(dataset.training[0].subject.get_image_path())
-
-    # Binarize images
-    reference_image = morphological_transformation(reference_image)
-    subject_image = morphological_transformation(subject_image)
-
-    reference_image = binarize_image(reference_image)
-    subject_image = binarize_image(subject_image)
-
-    # Show images
-    plt.subplot(121), plt.imshow(reference_image, cmap='gray')
-    plt.title('Reference Image')
-
-    plt.subplot(122), plt.imshow(subject_image, cmap='gray')
-    plt.title('Subject Image')
-    plt.show()
+    # Technique 1
+    technique_1(dataset)
 
 
 # Only if this file is run directly
