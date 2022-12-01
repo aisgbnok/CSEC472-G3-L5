8fd0007cf50a0a97edab1afe0236e20db7f455f5
Anthony Swierkosz <anthony@swierkosz.dev>
2022-11-30 19:24:10 -0500

    Rework main to include user menu and improve expandability

--- main.py
+++ main.py
@@ -2,18 +2,89 @@
 CSEC-472 Bonus Lab 5
 Author: Anthony Swierkosz
 
-
+Main program for the bonus lab.
+Brings everything together and lets you run selected components.
 """
+from enum import Enum
 from os import path
+from textwrap import wrap
 
 from dataset import Dataset
 
 
-def main():
-    # Load data
+class MenuStyle(Enum):
+    """
+    Enum for the menu style.
+    """
+    FULL = 0
+    WELCOME = 1
+    MENU = 2
+
+
+def bold(text):
+    """
+    Return the text in bold.
+
+    :param text: Text to bold.
+    :return: Bolded text.
+    """
+    return f"\033[1m{text}\033[0m"
+
+
+def print_menu(style=MenuStyle.FULL):
+    """
+    Print the welcome message.
+
+    :return: None
+    """
+    title = "CSEC 472: Lab 5"
+    subtitle = "Group 3"
+    width = len(title) + 24 if len(title) > len(subtitle) else len(subtitle) + 24
+    # description = " ##\n## ".join(wrap("Choose one of the options below to run a component of the lab.", width - 4))
+    description = "\n".join(wrap("Choose one of the options below to run a component of the lab.", width))
+
+    if style == MenuStyle.FULL or style == MenuStyle.WELCOME:
+        print(f'{"":#^{width}}')
+        print(f'##{f" {bold(title)} ":^{width + 4}}##')
+        print(f'##{f" {subtitle} ":^{width - 4}}##')
+        print(f'{"":#^{width}}\n')
+
+    if style == MenuStyle.FULL or style == MenuStyle.MENU:
+        print(f'## User Menu\n{description}\n')
+
+        print("0. Exit")
+        print("1. Run Anthony's individual components.")
+
+
+def load_data():
+    """
+    Load the dataset into a Dataset object.
+
+    :return: None
+    """
+    # Get the path to the dataset
     base_path = path.dirname(__file__)
     dataset_path = path.abspath(path.join(base_path, "sd04", "png_txt"))
+
+    # Load the dataset
+    print("## Generating the Dataset")
     dataset = Dataset(dataset_path)
+    dataset.populate_dataset(print_progress=True)
+    print()
+    return dataset
+
+
+def main():
+    print_menu(MenuStyle.WELCOME)
+
+    # Load Dataset
+    dataset = load_data()
+
+    # Handle user menu selection
+    user_response = "-1"
+    while int(user_response.strip()) != 0:
+        print_menu(MenuStyle.MENU)
+        user_response = input("Enter your choice: ")
 
 
 # Only if this file is run directly
